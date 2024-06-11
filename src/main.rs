cb6532a3880e51c76ed1947bca9151b1e7383fca
Madeline <oficialclankyrblx@gmail.com>
2024-06-10 21:07:00 -0600

    final

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,81 @@
 use std::env;
-use std::io::{self, stdin,stdout,Write};
+use std::io::{stdin, stdout, Write};
 use std::path::Path;
-use std::process::Command;
-use std::str::SplitWhitespace;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
     loop {
-        let current_dir = env::current_dir().unwrap();
-        print!(">  {}", current_dir.display());
-        let _ = stdout().flush();
+        // use the `>` character as the prompt
+        // need to explicitly flush this to ensure it prints before read_line
+        print!("> ");
+        stdout().flush().unwrap();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        // read_line leaves a trailing newline, which trim removes
+        // this needs to be peekable so we can determine when we are on the last command
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        match command {
-            "cd" => {
-                match change_directory(args.clone()) {
-                    Ok(()) => continue,
-                    Err(e) => println!("error: {}", e),
+        while let Some(command) = commands.next() {
+            // everything after the first whitespace character is interpreted as args to the command
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    // default to '/' as new directory if one was not provided
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
                 }
-            },
-           "exit" => return,
-            command => {
-                let child = Command::new(command)
-                    .args(args)
-                    .spawn();
+                "exit" => return,
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
 
-                match child {
-                    Ok(mut child) => { child.wait(); },
-                    Err(e) => eprintln!("error: {}", e),
+                    let stdout = if commands.peek().is_some() {
+                        // there is another command piped behind this one
+                        // prepare to send output to the next command
+                        Stdio::piped()
+                    } else {
+                        // there are no more commands piped behind this one
+                        // send output to shell stdout
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
                 }
             }
         }
+
+        if let Some(mut final_command) = previous_command {
+            // block until the final command has finished
+            final_command.wait().unwrap();
+        }
     }
 }
-
 fn change_directory(args: SplitWhitespace) -> io::Result<()>  {
     let new_dir = args.peekable().peek().map_or("/", |x| *x);
     let root = Path::new(new_dir);
